15a2886cca76a7c1e1a1177be6ec964eda1d5591
shoeb512 <125538271+shoeb4dev@users.noreply.github.com>
2026-08-04 16:46:10 +0530

    added change

---
+++ Easy.js
@@ -0,0 +1,33 @@
+let Name = "Shoeb khan";
+
+console.log("hello friend my name is "+ Name)
+
+
+let num = 89;
+
+if(num%2 == 0){
+    console.log("this is even number");
+    
+}else{
+    console.log("this is odd number");
+    
+}
+
+
+let Age = 9;
+
+if(Age >= 18){
+    console.log("You are eligible to vote");
+}else{
+    console.log("You are not eligible to vote");
+    
+}
+
+
+let data = "JavaScript";
+
+let arr = data.split("").reverse();
+
+let result = arr.join("");
+
+console.log(result);
