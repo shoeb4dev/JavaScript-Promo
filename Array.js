72f3bcccfb73458b5f49fb3db27c86fc6b1fff53
shoeb512 <125538271+shoeb4dev@users.noreply.github.com>
2026-08-11 16:14:49 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -1,23 +1,29 @@
 let Number = [ 10,20,30,40,50,60,70,80,90,100];
 
 // Got the last index of Array Number
-console.log(Number[Number.length - 1]);
+// console.log(Number[Number.length - 1]);
 
 let num2 =[];
 
 // Add something in the end of array
 num2.push(10)
-console.log(num2);
+// console.log(num2);
 
 //Remove  index value in the end of array
 Number.pop();
 
-console.log(Number);
+// console.log(Number);
 
 Number.unshift(5);
 
-console.log("Added value in the beginning: " + Number);
+// console.log("Added value in the beginning: " + Number);
 
 Number.shift()
 
-console.log("removing number from beginnig " + Number);
+// console.log("removing number from beginnig " + Number);
+
+
+for (let i = 7; i < Number.length; i++){
+
+    console.log(Number[i]);
+}
