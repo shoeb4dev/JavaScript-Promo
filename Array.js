ec7f66e35055c20f1ff2e773c5f88525a19d6521
shoeb512 <125538271+shoeb4dev@users.noreply.github.com>
2026-08-14 16:24:01 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -1,12 +1,12 @@
-let Number = [ 10,20,30,40,50,60,70,80,90,100];
+let Number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
 // Got the last index of Array Number
 // console.log(Number[Number.length - 1]);
 
-let num2 =[];
+let num2 = [];
 
 // Add something in the end of array
-num2.push(10)
+num2.push(10);
 // console.log(num2);
 
 //Remove  index value in the end of array
@@ -18,21 +18,18 @@ Number.unshift(5);
 
 // console.log("Added value in the beginning: " + Number);
 
-Number.shift()
+Number.shift();
 
 // console.log("removing number from beginnig " + Number);
 
-
-for (let i = 7; i < Number.length; i++){
-
-    console.log(Number[i]);
+for (let i = 7; i < Number.length; i++) {
+  console.log(Number[i]);
 }
-
 
 const numbers = [1, 2, 3, 4, 5];
 
 const result = numbers.map(function (num) {
-    return num * 2;
+  return num * 2;
 });
 
 console.log(result);
