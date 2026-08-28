b5b802410efdabd831225b7061a3a075cc433d01
shoeb512 <125538271+shoeb4dev@users.noreply.github.com>
2026-08-28 19:55:02 +0530

    Create CallBack.js

---
+++ CallBack.js
@@ -0,0 +1,21 @@
+function test(cbtest) {
+    return cbtest()   
+
+
+}
+
+test(function (){
+    console.log("hello Shoeb");
+    
+})
+
+
+function test2(name,cb2) {
+    return cb(name);
+    
+}
+
+test2(Shoeb , function(){
+        console.log(name);
+        
+})
